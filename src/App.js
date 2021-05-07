0547cf74f2de8cec4b2784fc4e25fe06c1e8f234
mentalaffect <mentalaffectdota@gmail.com>
2021-05-07 23:33:44 +0300

    v5.0.0

--- src/App.js
+++ src/App.js
@@ -1,25 +1,147 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react";
+import {useLocalStore} from "utils/hooks/useLocal";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import Auth from "pages/Auth";
+import Creator from "pages/Creator";
+import Choice from "pages/Choice";
+import SpawnChoice from "pages/SpawnChoice";
+import Bank from "pages/Bank";
+import HUD from "pages/HUD";
+import AdminRedactor from "pages/AdminRedactor";
+import BattlePass from "pages/BattlePass";
+import ATM from "pages/ATM";
+
+import Chat from "pages/HUD/components/Chat/Chat";
+
+import AuthStore from "store/AuthStore";
+import BankStore from "store/BankStore";
+import ChatStore from "store/ChatStore";
+import CreatorStore from "store/CreatorStore";
+import HUDStore from "store/HUDStore";
+import CrimeHUDStore from "store/CrimeHUDStore";
+import BattlePassStore from "store/BattlePassStore";
+
+import spawnsData from "configs/spawnData";
+
+const App = () => {
+    const authStore = useLocalStore(() => new AuthStore());
+    const bankStore = useLocalStore(() => new BankStore());
+    const chatStore = useLocalStore(() => new ChatStore());
+    const creatorStore = useLocalStore(() => new CreatorStore());
+    const hudStore = useLocalStore(() => new HUDStore());
+    const crimeHudStore = useLocalStore(() => new CrimeHUDStore());
+    const battlePassStore = useLocalStore(() => new BattlePassStore());
+
+    const [component, setComponent] = React.useState(null),
+        [isRegistered, setRegistered] = React.useState(false),
+        [characters, setCharacters] = React.useState([]),
+        [adminRedactorData, setAdminRedactorData] = React.useState([]),
+        [currentHUD, setHUD] = React.useState(0),
+        [isCursorActive, setCursorActive] = React.useState(false);
+
+    React.useEffect(() => {
+        window.window.alt.emit('client::cef:ready');
+
+        window.window.alt.on('cef::auth:start', bool => {
+            setRegistered(bool);
+            setComponent('auth');
+        });
+
+        window.alt.on('cef::characterCustom:start', () => {
+            setComponent('creator');
+        });
+
+        window.alt.on('cef::characterChoice:start', array => {
+            setCharacters(array);
+            setComponent('choice');
+        });
+
+        window.alt.on('cef::spawnChoice:start', data => {
+            spawnsData[2].data = data.homes;
+            spawnsData[3].data = data.array.frak;
+            spawnsData[4].data = data.array.fam;
+            setComponent('spawnChoice');
+        });
+
+        window.alt.on('cef::bank:start', () => {
+            setComponent('bank')
+        });
+
+        window.alt.on('cef::hud:start', () => {
+            setComponent('hud');
+        });
+        window.alt.on('cef::hud:change', int => setHUD(int));
+
+        window.alt.on('cef::adminRedactor:start', array => {
+            setAdminRedactorData(array);
+            setComponent('adminRedactor');
+        });
+
+        window.alt.on('cef::battlePass:start', () => {
+            setComponent('battlePass');
+        });
+
+        window.alt.on('cef::cursor:change', bool => setCursorActive(bool));
+    }, []);
+    React.useEffect(() => {
+        window.alt.on('cef::auth:getResetCode', (code) => {
+            authStore.fetchResetCode(code);
+        })
+    }, [authStore]);
+    React.useEffect(() => {
+        window.alt.on('cef::bank:setAccountData', obj => bankStore.fetchAccountState(obj));
+        window.alt.on('cef::bank:setFines', array => bankStore.fetchFines(array));
+        window.alt.on('cef::bank:changeFines', obj => bankStore.changeFines(obj));
+        window.alt.on('cef::bank:setCards', array => bankStore.fetchCards(array));
+        window.alt.on('cef::bank:setWithdrawHistory', array => bankStore.fetchWithdrawHistory(array));
+        window.alt.on('cef::bank:addWithdraw', data => bankStore.addWithdraw(data));
+        window.alt.on('cef::bank:setTopUpHistory', array => bankStore.fetchTopUpHistory(array));
+        window.alt.on('cef::bank:addTopUp', data => bankStore.addTopUp(data));
+    }, [bankStore]);
+    React.useEffect(() => {
+        window.alt.on('cef::hud:loadState', obj => hudStore.fetchPlayerState(obj));
+        window.alt.on('cef::hud:loadMoney', obj => hudStore.fetchPlayerMoney(obj));
+        window.alt.on('cef::hud:setMapState', obj => hudStore.fetchMapState(obj));
+        window.alt.on('cef::hud:setDate', date => hudStore.fetchDate(date));
+        window.alt.on('cef::hud:setTime', time => hudStore.fetchTime(time));
+        window.alt.on('cef::hud:loadCarState', obj => hudStore.fetchCarState(obj));
+        window.alt.on('cef::hud:heal', bool => hudStore.fetchEffects('healing', bool));
+        window.alt.on('cef::hud:kill', data => hudStore.fetchDeadState(data));
+        window.alt.on('cef::hud:setGreenZone', bool => hudStore.setGreenZone(bool));
+        window.alt.on('cef::hud:loadAmmo', obj => hudStore.fetchAmmo(obj));
+        window.alt.on('cef::hud:setCarHud', bool => hudStore.setCarHud(bool));
+    }, [hudStore]);
+    React.useEffect(() => {
+        window.alt.on('cef::chat:setAccess', obj => chatStore.fetchAccess(obj));
+        window.alt.on('cef::chat:loadFamilyIcon', icon => chatStore.fetchFamilyIcon(icon));
+    }, [chatStore]);
+    React.useEffect(() => {
+        window.alt.on('cef::creator:setData', data => creatorStore.fetchData(data));
+    }, [creatorStore]);
+    React.useEffect(() => {
+        window.alt.on('cef::battlePass:setQuests', data => battlePassStore.fetchQuests(data));
+        window.alt.on('cef::battlePass:setAwards', data => battlePassStore.fetchAwards(data));
+        window.alt.on('cef::battlePass:setSeason', name => battlePassStore.setSeason(name));
+        window.alt.on('cef::battlePass:setCoins', value => battlePassStore.setSeason(value));
+    }, [battlePassStore]);
+
+    return <div className='app'>
+        {component === 'auth' && <Auth isRegistered={isRegistered} store={authStore}/>}
+        {component === 'creator' && <Creator store={creatorStore}/>}
+        {component === 'choice' && <Choice characters={characters}/>}
+        {component === 'spawnChoice' && <SpawnChoice spawnData={spawnsData}/>}
+        {component === 'bank' && <Bank store={bankStore}/>}
+        {component === 'hud' && <HUD
+            defaultStore={hudStore}
+            crimeStore={crimeHudStore}
+            currentHUD={currentHUD}
+        />}
+        {component === 'adminRedactor' && <AdminRedactor data={adminRedactorData}/>}
+        {component === 'battlePass' && <BattlePass store={battlePassStore}/>}
+        {component === 'atm' && <ATM store={bankStore}/>}
+        <Chat store={chatStore} isCursorActive={isCursorActive}
+              isVisible={component === 'hud' && currentHUD === 0 && !hudStore.deadState.isDead}/>
     </div>
-  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+import 'utils/initMobX';
+
+import './styles/global.scss';
+
+if (window.alt === undefined) {
+    window.alt = {
+        emit: () => {},
+        on: () => {}
+    };
+}
 
 ReactDOM.render(
   <React.StrictMode>
@@ -10,8 +20,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
