a3e47fd8d711fdd34d73d3d2c5038435d9ee536b
mentalaffect <mentalaffectdota@gmail.com>
2021-06-16 17:58:51 +0300

    v6.5.3

--- src/pages/CrimeMenu/components/Events/Events.js
+++ src/pages/CrimeMenu/components/Events/Events.js
@@ -7,7 +7,7 @@ const Events = ({store}) => {
 	return <div className='crime-menu-events'>
 		<div className='crime-menu-events__title'>Мероприятия</div>
 		<div className='crime-menu-events-list'>
-			{store.events?.map((el, key) => {
+			{store.events.map((el, key) => {
 				return <div key={key} className='crime-menu-events-list-element' style={el.image ? {background: `url("./publicImages/crimeMenu/${el.image}")`} : {background: '#00000080'}}>
 					<div className='crime-menu-events-list-element__date'>{el.date}</div>
 					<div className='crime-menu-events-list-element__text'>{el.text}</div>
